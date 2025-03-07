555720c7f3f80d742b6aefff9d42398ca5f8a897
lgzzk <3088773042@qq.com>
2025-03-07 11:56:41 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text1">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+
+.text1{
+    @apply p-10 m-10 text-3xl font-bold underline text-amber-900;
+}
+
+</style>
